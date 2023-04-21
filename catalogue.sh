4686d613739ab554565b561c3d023f6054f76aa9
gmr <gmrkurma234@gmail.com>
2023-04-21 15:12:57 +0530

    config

--- catalogue.sh
+++ catalogue.sh
@@ -10,7 +10,7 @@ npm install
 cp /home/centos/Roboshop1/catalogue.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl enable catalogue
-systemctl start catalogue
+systemctl restart catalogue
 cp /home/centos/Roboshop1/mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org-shell -y
 mongo --host mongodb-dev.gmr.dev.online </app/schema/catalogue.js
