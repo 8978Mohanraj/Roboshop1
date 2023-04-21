5e55dde4d1b6d9200bf1d6b5281f236b0a0fb4fc
gmr <gmrkurma234@gmail.com>
2023-04-21 11:57:26 +0530

    config

--- frontend.sh
+++ frontend.sh
@@ -1,5 +1,5 @@
 yum install nginx -y
-cp robo.conf /etc/nginx/default.d/robo.conf
+cp Roboshop1.conf /etc/nginx/default.d/Roboshop1.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
