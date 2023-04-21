d512b9583d60dd90d7796e56af7d7829334136d2
gmr <gmrkurma234@gmail.com>
2023-04-21 13:11:08 +0530

    config

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 yum install mongodb-org -y
-systemctl enable mongod
-systemctl start mongod
+systemctl enable mongodb
+systemctl start mongodb
 # I want to change the ip address(sed)
-systemctl restart mongod
+systemctl restart mongodb
