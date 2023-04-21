0cd72baab6ca6caf038b01a02eb5a80f813d53c5
gmr <gmrkurma234@gmail.com>
2023-04-21 11:32:24 +0530

    config

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,8 @@
 yum install nginx -y
+cp robo.conf /etc/nginx/default.d/robo.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-# Some files need to be created
 systemctl restart nginx
 systemctl enable nginx
